e12d41eb7ef590c36c866683cffc0bfca8423882
Mark Dalrymple <markd@bignerdranch.com>
2018-10-10 21:32:49 -0400

    Adds placeholder button.

--- Projector/ViewController.swift
+++ Projector/ViewController.swift
@@ -1,20 +1,13 @@
-//
-//  ViewController.swift
-//  Projector
-//
-//  Created by markd on 10/10/18.
-//  Copyright © 2018 Borkware. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func putStuffOnPröjector() {
+        print("Snorgle")
+    }
 }
 
