63340e3d9dbdb9e047e1ff293c8ad568578ac89c
Mark Dalrymple <markd@bignerdranch.com>
2018-10-10 22:25:55 -0400

    Adds a ProjectorContents view controller

---
+++ Projector/ProjectorContentsViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class ProjectorContentsViewController: UIViewController {
+    static let storyboardName = "ProjectorContents"
+
+    override func viewDidLoad() {
+        
+    }
+    
+}
