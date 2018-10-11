0ddb4d391cb6e2ad45cb7cf11e46e7ab83e14f93
Mark Dalrymple <markd@bignerdranch.com>
2018-10-10 20:57:26 -0400

    first post!

---
+++ Projector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projector
+//
+//  Created by markd on 10/10/18.
+//  Copyright © 2018 Borkware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
